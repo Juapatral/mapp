01286b3851bf114bc7092580f2029efabc32c533
Juan Pablo Trujillo Alviz <39378207+Juapatral@users.noreply.github.com>
2019-08-18 16:20:59 -0500

    Uploades MAPp scripts

---
+++ app.R
@@ -0,0 +1,380 @@
+### Prueba de mapas y top 10
+### juapatral
+### 2019-08-18
+
+### ---- paquetes ----
+
+library(shiny, quietly = T)
+library(leaflet, quietly = T)
+library(markdown, quietly = T)
+library(shinythemes, quietly = T)
+
+### ---- inicio ui ----
+
+ui <- navbarPage("MAPp", theme = shinytheme("simplex"),
+
+    ### ---- inicio pesanias ----
+    
+    #tabsetPanel(
+        
+        ### ---- pestania de inicio ----
+        tabPanel("Inicio", includeMarkdown("opening.md")),
+        
+        ### ---- pestania de comuna ----
+        
+        tabPanel("Comuna",
+                 
+                 # titulo del mapa
+                 titlePanel("Mapa por comuna"),
+                 
+                # mainPanel(
+                     
+                     # texto
+                     selectInput(inputId = "marcadores_cm",
+                                 label = "Mostrar marcadores",
+                                 choices = c("Si" = T, "No" = F)),
+                     
+                     # crear mapa
+                     leafletOutput("mapa_cm", width = "100%", height = 400)
+                     
+                #    )
+                 ),
+        
+        ### ---- pestania de barrio ----
+
+        tabPanel("Barrio",
+
+                #mainPanel(
+
+                         # titulo del mapa
+                         titlePanel("Mapa por barrio"),
+
+                         # texto
+                         selectInput(inputId = "marcadores_br",
+                                     label = "Mostrar marcadores",
+                                     choices = c("Si" = T, "No" = F)),
+
+                         # crear mapa
+                         leafletOutput("mapa_br", width = "100%", height = 400)
+
+                  #       )
+                ),
+
+        ### ---- pestania de top 10 ----
+        
+        tabPanel("Top",
+                 
+                 titlePanel("Principales"),
+                 
+                 #mainPanel(
+                     
+                     fluidRow(
+                         
+                         # tabla comunas
+                         column(width = 6, 
+                                h4("Tabla por comunas"),
+                                dataTableOutput("tabla_cm")
+                                ),
+                         
+                         # tabla barrios
+                         column(width = 6,
+                                h4("Tabla por barrios"),
+                                dataTableOutput("tabla_br")
+                                )
+                        )
+                     #)
+                 )
+        )
+    #)
+
+
+
+### ---- server ----
+
+server <- function(input, output) {
+
+    ### ---- cargar liberias ----
+    library(leaflet, quietly = T)
+    library(sf, quietly = T)
+    library(data.table, quietly = T)
+    library(dplyr, quietly = T)
+    library(shiny, quietly = T)
+    library(lubridate, quietly = T)
+    library(plotly, quietly = T)
+    library(readxl, quietly = T)
+    library(stringr, quietly = T)
+    
+    ### ---- cargar archivos ----
+    
+    # datos historicos
+    dir <- read_excel("files/DIRECCIONES 12-08-2019.xlsx",
+                      col_types = "text")
+    
+    # datos espaciales
+    barrio <- read_sf("files/Limite_Barrio_Vereda_Catastral/Limite_Barrio_Vereda_Catastral.shp")
+    comuna <- read_sf("files/Limite_Comunas_Corregimientos/Limite_Comunas_Corregimientos.shp")
+    
+    ### ---- crear tablas ----
+    
+    dir$lon <- as.numeric(dir$lon)
+    dir$lat <- as.numeric(dir$lat)
+    
+    # crear archivo para barrio
+    dir2 <- dir %>% 
+            mutate(comuna_barrio = str_sub(cbml, 1, 4)) %>% 
+            filter(!is.na(comuna_barrio)) %>% 
+            group_by(comuna_barrio) %>% 
+            summarize(total = n())
+    
+    # crear puntos para coordenadas
+    dir3 <- dir %>% 
+            filter(lon != 0) %>%
+            group_by(dirgoogle...11, lon, lat) %>% 
+            summarize(total = n())
+    
+    # crear archivo de comunas
+    dir4 <- dir %>% 
+            mutate(comuna = str_sub(cbml, 1, 2)) %>% 
+            filter(!is.na(comuna)) %>% 
+            group_by(comuna) %>% 
+            summarize(total = n())
+    
+    # crear archivo mapa de barrios
+    nuevo_mapa <- inner_join(barrio, 
+                             dir2,
+                             by = c("CODIGO" = "comuna_barrio"))
+    
+    # crear archivo mapa de comunas
+    nuevo_mapa_cm <- inner_join(comuna, 
+                                dir4,
+                                by = c("COMUNA" = "comuna"))
+    
+    ### ---- crear outputs ----
+    
+    # crear mapa de barrio
+    output$mapa_br <- renderLeaflet({
+        
+    # establecer paleta
+        mypal <- colorNumeric(palette = "magma", 
+                               domain = nuevo_mapa$total,
+                               reverse = TRUE)
+        
+        # crear mapa
+        
+        # agregar marcadores
+            if(input$marcadores_br){
+                
+                leaflet() %>%
+                    
+                addPolygons(data = nuevo_mapa,
+                            color = "grey",
+                            opacity = 0.9,
+                            weight = 1, # grosor de la linea
+                            fillColor = ~mypal(total),
+                            fillOpacity = 0.6,
+                            label = ~NOMBRE_BAR,
+                            # ajustar sombreado de seleccion
+                            highlightOptions = highlightOptions(color = "black",
+                                                                weight = 3, 
+                                                                bringToFront = T, 
+                                                                opacity = 1),
+                            # ajustar descripcion emergente al darle click
+                            popup = paste("Barrio: ", 
+                                          nuevo_mapa$NOMBRE_BAR, 
+                                          "<br>",
+                                          "Total: ", 
+                                          nuevo_mapa$total, 
+                                          "<br>")
+                            ) %>%
+                
+                # agregar marcadores
+                addMarkers(~lon, 
+                           ~lat, 
+                           data = dir3, 
+                           popup = ~as.character(total),
+                           label = ~as.character(dirgoogle...11)) %>%
+                
+                #establecer mapa de fondo
+                addProviderTiles(providers$Wikimedia) %>%
+                
+                # agregar leyenda
+                addLegend(position = "bottomright", 
+                          pal = mypal, 
+                          values = nuevo_mapa$total,
+                          title = "Total",
+                          opacity = 0.3)
+            }else{
+             
+                leaflet() %>%
+                    
+                addPolygons(data = nuevo_mapa,
+                            color = "grey",
+                            opacity = 0.9,
+                            weight = 1, # grosor de la linea
+                            fillColor = ~mypal(total),
+                            fillOpacity = 0.6,
+                            label = ~NOMBRE_BAR,
+                            # ajustar sombreado de seleccion
+                            highlightOptions = highlightOptions(color = "black",
+                                                                weight = 3, 
+                                                                bringToFront = T, 
+                                                                opacity = 1),
+                            # ajustar descripcion emergente al darle click
+                            popup = paste("Barrio: ", 
+                                          nuevo_mapa$NOMBRE_BAR, 
+                                          "<br>",
+                                          "Total: ", 
+                                          nuevo_mapa$total, 
+                                          "<br>")
+                            ) %>%
+                
+                #establecer mapa de fondo
+                addProviderTiles(providers$Wikimedia) %>%
+                
+                # agregar leyenda
+                addLegend(position = "bottomright", 
+                          pal = mypal, 
+                          values = nuevo_mapa$total,
+                          title = "Total",
+                          opacity = 0.3)   
+            }
+    })
+    
+    # crear mapa de comuna
+    output$mapa_cm <- renderLeaflet({
+        
+    # establecer paleta
+        mypal_cm <- colorNumeric(palette = "magma", 
+                                 domain = nuevo_mapa_cm$total,
+                                 reverse = TRUE)
+        
+        # crear mapa
+        
+        # agregar marcadores
+            if(input$marcadores_cm){
+                
+                leaflet() %>%
+                    
+                addPolygons(data = nuevo_mapa_cm,
+                            color = "grey",
+                            opacity = 0.9,
+                            weight = 1, # grosor de la linea
+                            fillColor = ~mypal_cm(total),
+                            fillOpacity = 0.6,
+                            label = ~NOMBRE,
+                            # ajustar sombreado de seleccion
+                            highlightOptions = highlightOptions(color = "black",
+                                                                weight = 3, 
+                                                                bringToFront = T, 
+                                                                opacity = 1),
+                            # ajustar descripcion emergente al darle click
+                            popup = paste("Comuna: ", 
+                                          nuevo_mapa_cm$NOMBRE, 
+                                          "<br>",
+                                          "Total: ", 
+                                          nuevo_mapa_cm$total, 
+                                          "<br>")
+                            ) %>%
+                
+                # agregar marcadores
+                addMarkers(~lon, 
+                           ~lat, 
+                           data = dir3, 
+                           popup = ~as.character(total),
+                           label = ~as.character(dirgoogle...11)) %>%
+                
+                #establecer mapa de fondo
+                addProviderTiles(providers$Wikimedia) %>%
+                
+                # agregar leyenda
+                addLegend(position = "bottomright", 
+                          pal = mypal_cm, 
+                          values = nuevo_mapa_cm$total,
+                          title = "Total",
+                          opacity = 0.3)
+            }else{
+             
+                leaflet() %>%
+                    
+                addPolygons(data = nuevo_mapa_cm,
+                            color = "grey",
+                            opacity = 0.9,
+                            weight = 1, # grosor de la linea
+                            fillColor = ~mypal_cm(total),
+                            fillOpacity = 0.6,
+                            label = ~NOMBRE,
+                            # ajustar sombreado de seleccion
+                            highlightOptions = highlightOptions(color = "black",
+                                                                weight = 3, 
+                                                                bringToFront = T, 
+                                                                opacity = 1),
+                            # ajustar descripcion emergente al darle click
+                            popup = paste("Comuna: ", 
+                                          nuevo_mapa_cm$NOMBRE, 
+                                          "<br>",
+                                          "Total: ", 
+                                          nuevo_mapa_cm$total, 
+                                          "<br>")
+                            ) %>%
+                
+                #establecer mapa de fondo
+                addProviderTiles(providers$Wikimedia) %>%
+                
+                # agregar leyenda
+                addLegend(position = "bottomright", 
+                          pal = mypal_cm, 
+                          values = nuevo_mapa_cm$total,
+                          title = "Total",
+                          opacity = 0.3)   
+            }
+    })
+    
+    # crear tabla de comuna
+    output$tabla_cm <- renderDataTable({
+        
+        # crear dataframe
+        com10 <- select(as.data.frame(comuna), "NOMBRE", "COMUNA") %>%
+                 inner_join(dir4, by = c("COMUNA" = "comuna")) %>%
+                 select(NOMBRE, total) %>%
+                 arrange(desc(total))
+        
+        # cambiar nombres
+        names(com10) <- c("Comuna", "Total de observaciones")
+        
+        # mostrar dataframe
+        as.data.frame(com10) 
+        
+        
+    }, 
+    
+    # elegir los primeros 10
+    options = list(pageLength = 10, width = 4)
+    )
+    
+    # crear tabla de barrio
+    output$tabla_br <- renderDataTable({
+        
+        # crear dataframe
+        bar10 <- select(as.data.frame(barrio), 
+                        NOMBRE_BAR, NOMBRE_COM, CODIGO) %>%
+                 inner_join(dir2, by = c("CODIGO" = "comuna_barrio")) %>%
+                 select(NOMBRE_COM, NOMBRE_BAR, total) %>%
+                 arrange(desc(total))
+        
+        # cambiar nombres
+        names(bar10) <- c("Comuna", "Barrio", "Total de observaciones")
+                    
+        # mostrar dataframe
+        as.data.frame(bar10)
+        
+        
+    }, 
+    
+    # elegir los primeros 10
+    options = list(pageLength = 10, width = 4)
+    )
+    
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
---
+++ opening.md
@@ -0,0 +1,39 @@
+# ***MAPp***
+
+Georreferenciación de algunas direcciones utilizando *R* y *leaflet*. 
+---
+
+## **Mapa del sitio**
+
+* En *`Inicio`*, la pestaña actualmente activa, encontrará el mapa del sitio y una breve explicación.
+
+* En *`Mapa por comuna`* encontrará un mapa interactivo de Medellín con la información por comunas.
+
+* En *`Mapa por barrio`* encontrará un mapa interactivo de Medellín con la información por barrios.
+
+* En *`Top`* encontrará unas tablas con las comunas y barrios donde más hay información y donde hay menos información, donde puede elegir entre mostrar las primeras 5, 10, 25, 50 o 100 y organizar alfabéticamente por nombre de comuna o barrio, u organizar por cantidad de observaciones. 
+
+## **Explicación de los mapas**
+
+Los mapas están compuestos de dos tipos de gráficos, marcadores y mapa de densidad. 
+
+Los *marcadores* consisten en la ubicación de las direcciones en el sistema de coordenadas internaciónal [wgs84](https://en.wikipedia.org/wiki/World_Geodetic_System). Al pasar por encima de los marcadores se muestra la dirección y la ciudad y al darle click se muestra la cantidad de observaciones en el conjunto de datos. La opción de mostrar marcadores se puede habilitar en cada pestaña. 
+
+El *mapa de densidad* por barrios o comunas solo está disponible para Medellín y consiste en generar una paleta de colores de acuerdo con la cantidad de observaciones, donde colores más claros indican menos observaciones y colores más oscuros más observaciones. Al darle click se muestra el nombre de la comuna o el barrio y la cantidad de observaciones. 
+
+## **Metodología**
+
+La georreferenciación se hizo posible a través de *Google Maps* y la [*API*](https://www.medellin.gov.co/geomedellin/index.hyg#openModal) de la Alcaldía de Medellín, trabajo realizado de manera manual. En caso de incrementar el tamaño del conjunto de datos, se evaluará la opción de utilizar la *API Geolocation* de *Google*. 
+
+Los mapas de comunas y barrios de Medellín están disponibles en su [sitio oficial.](https://geomedellin-m-medellin.opendata.arcgis.com/)
+
+***Nota:*** no todas las observaciones que son de Medellín se pudieron asociar a un barrio o comuna, por lo que se requiere un trabajo manual para hacerlo.
+
+---
+<font size = "1">
+
+Version *MAPp*: 2019.08.18
+
+Maintained by: juapatral
+
+</font>
